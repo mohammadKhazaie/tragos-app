1ef384657189ccbb301b42226b115970b141c3dc
mohammadKhazaie <mohammad.khazaie010@gmail.com>
2026-07-30 09:46:04 +0330

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
    ⚠️ این فایل رو بعد از ساخت پروژه Supabase پر کن
 ═══════════════════════════════════════════ */
 
-const SUPABASE_URL = 'https://muwigfufyqqoeqbqeomj.supabase.co/auth/v1/.well-known/jwks.json';
+const SUPABASE_URL = 'https://muwigfufyqqoeqbqeomj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11d2lnZnVmeXFxb2VxYnFlb21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzODA4OTUsImV4cCI6MjEwMDk1Njg5NX0.1vlIKjh5hVZe38MXbk-FaIKyvmMJyNvQnddfR-oGfS0';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
